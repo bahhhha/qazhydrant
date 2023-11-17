0234fa8955a3f23bbded13764bfeb4cd689fb057
mintjulep21 <zharylkassyn001@gmail.com>
2023-11-18 03:22:47 +0600

    added media

--- qazhydrant/src/components/Media.jsx
+++ qazhydrant/src/components/Media.jsx
@@ -31,16 +31,16 @@ const Article = ({ article }) => {
       <div className="flex gap-4 mediabg rounded-full border-2 border-main border-opacity-50 drop-shadow-sm w-[960px] m-auto  justify-start items-center ">
         <img
           src={article.img}
-          className="md:w-24 md:h-24 object-cover rounded-full "
+          className="md:w-36 md:h-36 object-cover rounded-full "
         ></img>
-        <div className="w-full flex items-center flex-col justify-start">
+        <div className="w-full text-left flex flex-col justify-start">
           <a
             href={article.link}
             className=" text-main font-semibold hover:text-[#bf7c2a] duration-150"
           >
             {article.text}
           </a>
-          <div className="text-right px-5 py-2">{article.author}</div>
+          <div className="text-left py-2">{article.author}</div>
         </div>
       </div>
     </div>
