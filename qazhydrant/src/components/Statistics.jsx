983c8444aad1c5ae705854a59f15fae234829470
mintjulep21 <zharylkassyn001@gmail.com>
2023-11-17 06:23:14 +0600

    hydrant pictures added

--- qazhydrant/src/components/Statistics.jsx
+++ qazhydrant/src/components/Statistics.jsx
@@ -2,12 +2,32 @@ const Statistics = () => {
   return (
     <div className="relative  bottom-6 contactbg w-[960px] m-auto border-main border-2 rounded-xl border-opacity-30 drop-shadow-2xl text-[#3e3e3e] flex justify-center">
       <div className=" py-4">
-        <div className="flex justify-center gap-24 items-center">
+        <div className="flex justify-center gap-24 py-8">
+          <div className="flex flex-col text-center ">
+            <img
+              src="images/underground.png"
+              className="w-64 rounded-md drop-shadow-xl border-2 border-main"
+            ></img>
+            <p className="py-4">
+              <span className="font-bold text-main">Underground</span> Hydrant
+            </p>
+          </div>
+          <div className="flex flex-col text-center ">
+            <img
+              src="images/normal.png"
+              className="w-64 rounded-md drop-shadow-xl border-2 border-main"
+            ></img>
+            <p className="py-4">
+              <span className="font-bold text-main">Ground</span> Hydrant
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center gap-24 items-center pb-10">
           <div>
             <p className="font-bold py-5 text-3xl text-left tracking-wide">
               A <span className="text-main">Real World</span> Problem
             </p>
-            <p className="text-justify w-[360px] ">
+            <p className="text-justify w-[480px] ">
               In around 14 countries globally,{" "}
               <span className="font-bold">
                 firefighters spend a significant amount of time—approximately
@@ -15,9 +35,6 @@ const Statistics = () => {
               </span>{" "}
               due to the use of underground hydrants.
             </p>
-          </div>
-          <div>
-            <img src="images/hydrants.png" className="w-64"></img>
           </div>
         </div>
       </div>
